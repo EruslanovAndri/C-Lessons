1e1d91152aa49ea3e10b696a4f2567698bd5ba06
EruslanovAndrei <eruslanov84as@gmail.com>
2022-09-05 20:04:37 +0300

    add Нахождение максимального элемента в массиве

---
+++ Lesson003FindMaxElementInArray/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+int[] numbers = { 2, 5, 6, 12, 8, 999 }; // задаем массив из чисел типа int
+int size = numbers.Length; // определяем длину массива 
+int index = 0; // инициализируем счетчик под названием index
+int max = numbers[index]; //присваиваем первому элементу под индесом 0 максимальное значение.
+
+while (index < size)
+{
+    if (numbers[index] > max)
+    {
+        max = numbers[index];
+    }
+    index++;
+}
+Console.WriteLine(max);

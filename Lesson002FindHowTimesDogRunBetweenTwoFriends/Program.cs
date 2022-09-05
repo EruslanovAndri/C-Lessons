f7cedb0696dff44ec22f8bb59a1aed393738fd71
EruslanovAndrei <eruslanov84as@gmail.com>
2022-09-05 18:46:54 +0300

    Two friends and a dog, find how many times the dog run between two friends

---
+++ Lesson002FindHowTimesDogRunBetweenTwoFriends/Program.cs
@@ -0,0 +1,35 @@
+﻿int count = 0;
+double distance = 10000;
+int firstFriendSpeed = 2;
+int secondFriendSpeed = 3;
+int dogSpeed = 5;
+int meetingPoint = 10;
+int friend = 2;
+double time = 0;
+
+if (distance > meetingPoint && friend != 2)
+{
+    time = distance / (secondFriendSpeed + dogSpeed);
+    distance = distance - (firstFriendSpeed + secondFriendSpeed) * time;
+    friend = 1;
+    count++;
+}
+else
+{
+    time = distance / (firstFriendSpeed + dogSpeed);
+    distance = distance - (firstFriendSpeed + secondFriendSpeed) * time;
+    friend = 2;
+    count++;
+}
+
+Console.WriteLine($"{count}");
+
+
+
+
+// Do not work
+
+
+
+
+

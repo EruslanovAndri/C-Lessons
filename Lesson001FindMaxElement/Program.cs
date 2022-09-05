ab5a52942f36f66ff0362f9631112a5569215d88
EruslanovAndrei <eruslanov84as@gmail.com>
2022-09-05 16:21:27 +0300

    add  блок - схему и написал код для поиска максимального числа

--- Lesson001FindMaxElement/Program.cs
+++ Lesson001FindMaxElement/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+int a = 16;
+int b = 30;
+int c = 45;
+int d = 56;
+int e = 88;
+int max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+Console.WriteLine(max);
+
+
+
+

445d238a6e40f89dd156f69db6723185dd39341f
EruslanovAndrei <eruslanov84as@gmail.com>
2022-09-05 20:53:17 +0300

    Add Scalar Product Vectors

---
+++ Lesson004ScalarProduct/Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+Console.Clear();
+
+int[] firstVector = { 1, 8, 7 };
+int[] secondVector = { 4, 9, 5 };
+int size = firstVector.Length;
+int scalarProduct = 0;
+int index = 0;
+
+
+while (index < size)
+{
+    scalarProduct = scalarProduct + firstVector[index] * secondVector[index];
+    index++;
+}
+
+Console.WriteLine(scalarProduct);

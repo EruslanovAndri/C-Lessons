4c319196292c253e210169203a30a25dd2c232fd
EruslanovAndrei <eruslanov84as@gmail.com>
2022-09-08 17:03:18 +0300

    add Find Max function

---
+++ Find_Max_Function/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+int[] array = {1,3,63,7,3,1,4};
+int FindMaxInArray(int[] array)
+{
+    int size = array.Length;
+    int index = 0;
+    int max = array[0];
+    while(index < array.Length)
+    {
+        if(array[index] > max)
+        {
+            max = array[index];
+        }
+        index++;
+    }
+    return max;
+}
+
+int res = FindMaxInArray(array);
+Console.WriteLine(res);

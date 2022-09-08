a89a4513da3736b21fcfd0d8d9d86e647a376a44
EruslanovAndrei <eruslanov84as@gmail.com>
2022-09-08 19:39:42 +0300

    Add Find sum of even numbers from 1 to 15

---
+++ Find_Sum_of_All_even_Numbers_from_1_to_15/Program.cs
@@ -0,0 +1,13 @@
+﻿
+Console.Clear();
+int didgit = 2;
+int sum = 0;
+
+while(didgit <= 15)
+{
+    if(didgit % 2  == 0)
+    sum += didgit;
+    didgit = didgit + 1;
+}
+Console.WriteLine(sum);
+// WORK

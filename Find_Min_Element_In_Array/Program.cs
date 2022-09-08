f3369a07f77f74ad7d6a2c1e6436ef7b6f38d0d3
EruslanovAndrei <eruslanov84as@gmail.com>
2022-09-08 17:41:53 +0300

    add Find min in array

---
+++ Find_Min_Element_In_Array/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+int[] numbers = { 31, 5, 6, 6 };
+int size = numbers.Length;
+int index = 0;
+int min = numbers[0];
+
+while (index < size)
+{
+    if (numbers[index] < min)
+    {
+        min = numbers[index];
+
+    }
+    index++;
+}
+Console.WriteLine(min);

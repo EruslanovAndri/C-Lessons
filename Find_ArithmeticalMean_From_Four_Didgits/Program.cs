b33379e05e19d21398d45e69eb9e2ad3c37d5c11
EruslanovAndrei <eruslanov84as@gmail.com>
2022-09-08 21:22:32 +0300

    add Find arethmetical mean from four didgits

---
+++ Find_ArithmeticalMean_From_Four_Didgits/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+int[] array = { 13, 11, 1, 6 };
+int size = array.Length;
+int index = 0;
+double average = 0;
+
+while (index < size)
+{
+    average = average + array[index];
+    index = index + 1;
+}
+Console.WriteLine($"The arethmetical mean is = {average / index}");
+
+// Work

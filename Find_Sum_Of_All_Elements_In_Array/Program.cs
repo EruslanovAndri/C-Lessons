af256550e699d7780ec313f105e57e0810376fa4
EruslanovAndrei <eruslanov84as@gmail.com>
2022-09-08 18:18:51 +0300

    Add Find sum of all elements in array

---
+++ Find_Sum_Of_All_Elements_In_Array/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+int[] array = {1,3,4,7,8};
+int size = array.Length;
+int index = 0;
+int result = 0;
+
+while( index < size)
+{
+    result = result + array[index];
+    index = index + 1;
+}
+Console.WriteLine(result);
+
+// WORK

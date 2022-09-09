63f6e876556257bc306378cfba4e5efb875ea345
EruslanovAndrei <eruslanov84as@gmail.com>
2022-09-09 17:17:04 +0300

    Add Find max and min in one array

---
+++ Find_Max_and_MIn_Element_In_One_Array/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.Clear();
+
+int[] array = { 2, 4, 6, 23, 7, 8, 9};
+int size = array.Length;
+int i = 0;
+int max = array[i];
+int min = array[i];
+
+
+while (i < size)
+{
+    if (array[i] > max)
+    {
+        max = array[i];
+    }
+    else
+    {
+        if (array[i] < min)
+        {
+            min = array[i];
+        }
+    }
+    i = i + 1;
+}
+
+Console.WriteLine($" Max = {max}");
+Console.WriteLine($" Min = {min}");
+
+
+// Work

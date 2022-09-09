918d54456918441e7392900ef428b4678d945e8c
EruslanovAndrei <eruslanov84as@gmail.com>
2022-09-09 13:01:05 +0300

    Add Find Factorial from 1 to 10

---
+++ Find_Factorial_From_1_to_N/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+
+int n = 10;
+int factorial = 1;
+int size = 0;
+
+while (size < 10)
+{
+    if (n != 0)
+    {
+        factorial = factorial * n;
+        n = n - 1;
+    }
+    size = size + 1;
+}
+Console.WriteLine(factorial);

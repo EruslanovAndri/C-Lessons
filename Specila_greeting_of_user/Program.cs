5e53cc7f778a479636bc197d38d110f2e54df40a
EruslanovAndrei <eruslanov84as@gmail.com>
2022-09-13 21:03:53 +0300

    add Specila_greeting_of_user

---
+++ Specila_greeting_of_user/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.WriteLine("Fill your name:");
+string user_name = Console.ReadLine();
+
+if(user_name.ToLower() == "masha")
+{
+    Console.WriteLine($"Great! It's Masha");
+}
+else
+{
+    Console.WriteLine($"Hello, {user_name}");
+}

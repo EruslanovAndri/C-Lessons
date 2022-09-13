158a9cf8c500cb26aabe16f9c37faa9a1e72a07b
EruslanovAndrei <eruslanov84as@gmail.com>
2022-09-13 20:03:22 +0300

    add Random

--- Sum_of_two_didgit/Program.cs
+++ Sum_of_two_didgit/Program.cs
@@ -1,7 +1,11 @@
 ﻿Console.Clear();
-double didgit1 = 5.3;
-double didgit2 = 8.33;
-double sum = 0;
+int didgit1 = new Random().Next(1,10);
+Console.WriteLine(didgit1);
+int didgit2 = new Random().Next(1,10);
+Console.WriteLine(didgit2);
+int sum = 0;
+
+
 
 sum = didgit1 + didgit2;
 
